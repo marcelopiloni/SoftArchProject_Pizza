1a1ecbc45d8439126b86eabd10a4cfc023c5c5f3
Marcelo Piloni <marcelo.piloni@outlook.com>
2025-04-14 16:30:27 -0300

    Atualizando o app.js sem sessões

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const routes = require('./src/routes');
-require('./src/config/dbConnect');
+const connectToDatabase = require('./src/config/dbConnect');
+require('dotenv').config();
 
 const app = express();
 
 // Middleware para parse de JSON
 app.use(express.json());
+
+// Conectar ao banco de dados
+connectToDatabase()
+  .then(() => console.log('Banco de dados conectado'))
+  .catch(err => console.error('Erro na conexão com o banco de dados:', err));
 
 // Rotas da API
 app.use('/api', routes);
