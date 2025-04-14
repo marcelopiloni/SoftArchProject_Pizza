bfbd913c251422ed78252d9177b658733e793d77
Marcelo Piloni <marcelo.piloni@outlook.com>
2025-04-14 16:29:52 -0300

    Simplificando as rotas do usuário

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,21 +1,13 @@
 const express = require('express');
 const userController = require('../controllers/userController');
-const { authMiddleware, adminMiddleware } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
-// Rotas públicas
-router.post('/register', userController.register);
-router.post('/login', userController.login);
-router.post('/logout', userController.logout);
-
-// Rotas protegidas para usuários
-router.get('/profile', authMiddleware, userController.getCurrentUser);
-router.put('/profile/:id', authMiddleware, userController.updateUser);
-
-// Rotas administrativas
-router.get('/', authMiddleware, adminMiddleware, userController.getAllUsers);
-router.get('/:id', authMiddleware, adminMiddleware, userController.getUser);
-router.delete('/:id', authMiddleware, adminMiddleware, userController.deleteUser);
+// Rotas sem autenticação
+router.post('/', userController.register);
+router.get('/', userController.getAllUsers);
+router.get('/:id', userController.getUser);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
