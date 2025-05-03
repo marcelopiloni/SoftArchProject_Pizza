0f700230f8230185c9b21bc39573411ce63d96e6
Marcelo Piloni <marcelo.piloni@outlook.com>
2025-05-02 21:15:35 -0300

    index.jss

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const userRoutes = require('./userRoutes');
+const pizzaRoutes = require('./pizzaRoutes');
+const orderRoutes = require('./orderRoutes');
+const deliveryRoutes = require('./deliveryRoutes');
 
 const routes = express.Router();
 
 routes.use('/users', userRoutes);
+routes.use('/pizzas', pizzaRoutes);
+routes.use('/orders', orderRoutes);
+routes.use('/deliveries', deliveryRoutes);
 
 module.exports = routes;
