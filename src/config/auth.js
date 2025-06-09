8985fa70ab26846913f39a917c40f58886dbd497
Gabriel Nathan <gnfs.gabriel@gmail.com>
2025-06-09 20:25:40 -0300

    auth

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,6 @@
+const crypto = require('crypto');
+
 module.exports = {
-    JWT_SECRET: 'dummy-secret',
-    JWT_EXPIRATION: '24h'
-  };
+    JWT_SECRET: process.env.JWT_SECRET || crypto.randomBytes(32).toString('hex'),
+    JWT_EXPIRATION: process.env.JWT_EXPIRES_IN || '24h'
+};
